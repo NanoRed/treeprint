4fb6a08d8801524b22c672f6f4f3e7d5a8796761
RedAFD <radixholms@gmail.com>
2020-09-04 15:00:49 +0800

    update unit test

--- treeprint_test.go
+++ treeprint_test.go
@@ -1,6 +1,8 @@
 package treeprint
 
 import (
+	"errors"
+	"fmt"
 	"math/rand"
 	"testing"
 	"time"
@@ -8,77 +10,183 @@ import (
 
 func TestPrint(t *testing.T) {
 
-	getNumber := func() []int {
-		number := make([]int, 0)
-		for i := 0; i < 20; i++ {
-			rand.Seed(time.Now().UnixNano())
-			number = append(number, rand.Intn(100000))
-		}
-		t.Logf("\n%v", number)
-		return number
+	// binary tree test
+	number := make([]int, 0)
+	for i := 0; i < 20; i++ {
+		rand.Seed(time.Now().UnixNano())
+		number = append(number, rand.Intn(100000))
 	}
-
-	// binary search tree test
-	tree := &BinarySearchTree{}
-	for key, val := range getNumber() {
-		tree.Append(val, key)
+	tree := &BinaryTree{}
+	for _, val := range number {
+		tree.Append(val, nil)
 	}
 	t.Logf("\n%s", Sprint(tree.root))
 
-	// TODO() multi leaf node tree test
+	// multi leaf node tree test
+	tree2 := &MultiLeafTree{}
+	rand.Seed(time.Now().UnixNano())
+	_ = tree2.Append([]int{}, rand.Intn(100000))
+	for i := 0; i < 5; i++ {
+		_ = tree2.Append([]int{i}, rand.Intn(100000))
+	}
+	layer2Index := rand.Intn(5)
+	for i := 0; i < 5; i++ {
+		_ = tree2.Append([]int{layer2Index, i}, rand.Intn(100000))
+	}
+	layer3Index := rand.Intn(5)
+	for i := 0; i < 5; i++ {
+		_ = tree2.Append([]int{layer2Index, layer3Index, i}, rand.Intn(100000))
+	}
+	t.Logf("\n%s", Sprint(tree2.root))
 }
 
-// BinarySearchTree binary search tree for testing
-type BinarySearchTree struct {
-	root *BinarySearchNode
+
+// ------------------------------
+// binary tree struct for testing
+// ------------------------------
+
+// BinaryTree tree structure
+type BinaryTree struct {
+	root  *BinaryTreeNode
+	count uint
 }
 
-// BinarySearchNode binary search tree node
-type BinarySearchNode struct {
-	Key   int
-	Value interface{}
-	Left  *BinarySearchNode
-	Right *BinarySearchNode
+// BinaryTreeNode tree node structure
+type BinaryTreeNode struct {
+	Key    int
+	Value  interface{}
+	Height uint
+	Left   *BinaryTreeNode
+	Right  *BinaryTreeNode
 }
 
 // Append append a new node to the tree
-func (t *BinarySearchTree) Append(key int, val interface{}) {
-	update(&t.root, key, val)
-}
+func (t *BinaryTree) Append(key int, val interface{}) {
 
-func update(node **BinarySearchNode, key int, val interface{}) {
-	if *node == nil {
-		*node = &BinarySearchNode{
-			Key:   key,
-			Value: val,
+	// nodes that need to be increased in height
+	increaseHeight := make(map[*BinaryTreeNode]struct{})
+	increaseTracking := func(parent *BinaryTreeNode, selected *BinaryTreeNode, another *BinaryTreeNode) {
+		// if selected leaf node height equal or higher than the another one, than it means has to increase
+		if another == nil || (selected != nil && selected.Height >= another.Height) {
+			increaseHeight[parent] = struct{}{}
+		} else {
+			increaseHeight = make(map[*BinaryTreeNode]struct{})
 		}
-		return
-	} else if key == (*node).Key {
-		(*node).Value = val
-		return
 	}
-	if key < (*node).Key {
-		update(&(*node).Left, key, val)
+
+	// search node
+	current := &t.root
+	for *current != nil {
+		if key < (*current).Key {
+			increaseTracking((*current), (*current).Left, (*current).Right)
+			current = &(*current).Left
+		} else if key > (*current).Key  {
+			increaseTracking((*current), (*current).Right, (*current).Left)
+			current = &(*current).Right
+		} else {
+			break
+		}
+	}
+	if *current == nil {
+		*current = &BinaryTreeNode{
+			Key:    key,
+			Value:  val,
+			Height: 1,
+		}
+		t.count++
+		for node := range increaseHeight {
+			node.Height++
+		}
 	} else {
-		update(&(*node).Right, key, val)
+		(*current).Value = val
 	}
 }
 
 // GetKey implement treeprint
-func (n *BinarySearchNode) GetKey() interface{} {
+func (n *BinaryTreeNode) GetKey() interface{} {
 	return n.Key
 }
 
 // GetValue implement treeprint
-func (n *BinarySearchNode) GetValue() interface{} {
+func (n *BinaryTreeNode) GetValue() interface{} {
+	return fmt.Sprintf("(h:%d)", n.Height) // n.Value
+}
+
+// RangeNode implement treeprint
+func (n *BinaryTreeNode) RangeNode() chan TreeNode {
+	c := make(chan TreeNode, 2)
+	c <- n.Left
+	c <- n.Right
+	close(c)
+	return c
+}
+
+
+// ----------------------------------
+// multi-node tree struct for testing
+// ----------------------------------
+
+// MultiLeafTree multi-node tree structure
+type MultiLeafTree struct {
+	root  *MultiLeafTreeNode
+	count uint
+}
+
+// MultiLeafTreeNode multi-node tree node structure
+type MultiLeafTreeNode struct {
+	Value interface{}
+	Leaf []*MultiLeafTreeNode
+}
+
+// Append append a new node to the tree by index
+// pass []int{} to index means root node
+// pass []int{0} means the 1st leaf node of root
+// pass []int{0, 1} means the 2nd leaf node of the 1st leaf node of root
+// etc.
+func (t *MultiLeafTree) Append(index []int, val interface{}) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = errors.New("index out of range")
+		}
+	}()
+	entry := &t.root
+	layer := len(index)
+	for i := 0; i < layer; i++ {
+		if i == layer-1 {
+			if index[i] == len((*entry).Leaf) {
+				(*entry).Leaf = append((*entry).Leaf, nil)
+			}
+		}
+		entry = &(*entry).Leaf[index[i]]
+	}
+	if *entry == nil {
+		*entry = &MultiLeafTreeNode{
+			Value: val,
+		}
+		t.count++
+	} else {
+		(*entry).Value = val
+	}
+	return
+}
+
+// GetKey implement treeprint
+func (n *MultiLeafTreeNode) GetKey() interface{} {
+	return n.Value
+}
+
+// GetValue implement treeprint
+func (n *MultiLeafTreeNode) GetValue() interface{} {
 	return ""
 }
 
 // RangeNode implement treeprint
-func (n *BinarySearchNode) RangeNode() chan TreeNode {
-	c := make(chan TreeNode, 2)
-	c <- n.Left
-	c <- n.Right
+func (n *MultiLeafTreeNode) RangeNode() chan TreeNode {
+	count := len(n.Leaf)
+	c := make(chan TreeNode, count)
+	for i := 0; i < count; i++ {
+		c <- n.Leaf[i]
+	}
 	close(c)
 	return c
 }
